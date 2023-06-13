e6c0d1403498ec730ebbe4a1e7972f27d29e767b
ajith82 <ajith@sketchbrahma.com>
2023-06-13 12:03:34 +0530

    added more images,condition removed

--- script.js
+++ script.js
@@ -4,21 +4,15 @@ const height = window.innerHeight/2 - 200;
 
 
 window.addEventListener("scroll",() => {
-    var windowHeight = window.pageYOffset;
-    var scrollPos = window.scrollY || window.pageYOffset;
-    var value = scrollPos + 200 + 'px';
-    if(windowHeight > height) {
-        slideOne.style.transform = `translate( -${value}, 0)`;
-        slideTwo.style.transform = `translate( ${value}, 0)`;
-    }else if(windowHeight < height) {
-        slideOne.style.transform = `translate( 0, 0)`;
-        slideTwo.style.transform = `translate( 0, 0)`;
-    }
-    // if(windowHeight > height) {
-    //     slideOne.classList.add("slideLeft");
-    //     slideTwo.classList.add("slideRight");
-    // }else{
-    //     slideOne.classList.remove("slideLeft");
-    //     slideTwo.classList.remove("slideRight")
+    var scrollPos = window.pageYOffset;
+    var value = scrollPos * 1.6 + 'px';
+    slideOne.style.transform = `translateX( -${value})`;
+        slideTwo.style.transform = `translateX( ${value})`;
+    // if(scrollPos > height) {
+    //     slideOne.style.transform = `translateX( -${value})`;
+    //     slideTwo.style.transform = `translate( ${value}, 0)`;
+    // }else if(scrollPos < height) {
+    //     slideOne.style.transform = `translate( 0, 0)`;
+    //     slideTwo.style.transform = `translate( 0, 0)`;
     // }
 })
